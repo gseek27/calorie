1e3585b4067738b4070ed253aba04b23bf5329c0
gseek27 <goalseek27@gmail.com>
2024-06-12 00:27:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const admin = require('firebase-admin');
 
 const app = express();
 const port = process.env.PORT || 8080;
