83e99226ea5c34aeb53981c9c24c611912c47e66
gseek27 <goalseek27@gmail.com>
2024-06-12 01:53:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const port = process.env.PORT || 8080;
 
 var admin = require("firebase-admin"); // Use const for constant value
 
-var serviceAccount = require("./calorie-calc-92ec4-firebase-adminsdk-gj2b1-30a696cb36.json");
+//var serviceAccount = require("./calorie-calc-92ec4-firebase-adminsdk-gj2b1-30a696cb36.json");
 
 
 admin.initializeApp({
-  credential: admin.credential.cert(require("./calorie-calc-92ec4-firebase-adminsdk-gj2b1-e5b395db87.json"))
+  credential: admin.credential.cert(require("calorie-calc-92ec4-firebase-adminsdk-gj2b1-e5b395db87.json"))
 });
  
 
